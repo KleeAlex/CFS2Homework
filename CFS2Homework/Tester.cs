6c14e55d13500c1cbd70ebab4bd0f993b9f66be5
Alex Klee <kleealex34@outlook.com>
2022-02-01 17:27:35 -0600

    Students

---
+++ CFS2Homework/Tester.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassesLibrary;
+
+namespace TesterProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students s1 = new Students();
+            s1.FirstName = "Alex";
+            s1.LastName = "Klee";
+            s1.Id = "AGK1043";
+            s1.Gpa = Convert.ToSingle("3.1");
+
+            Console.WriteLine("Mr. {0} {1}, is a middeling student who's gpa sits at {2} ID for the year is {3}", s1.FirstName, s1.LastName, s1.Gpa, s1.Id);
+
+        }
+    }
+}
---
+++ ClassesLibrary/Students.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Students
+    {
+
+
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private float _gpa;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public float Gpa
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
+        }
+
+
+        public Students(string firstName, string lastName, string id , float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            Gpa = gpa;
+        }
+
+        public Students() { }
+
+        public override string ToString()
+        {
+            return string.Format("FirstName: {0}\nLastName: {1}\nId: {2}\nGpa: {3}", FirstName, LastName, Id, Gpa);
+        }
+
+
+
+
+
+
+    }
+}
