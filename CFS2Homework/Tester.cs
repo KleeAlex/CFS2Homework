3abb7e24d5a701ef4c20da37eb17a235300596c1
Alex Klee <kleealex34@outlook.com>
2022-02-01 23:22:08 -0600

    Vehicle Commit

--- CFS2Homework/Tester.cs
+++ CFS2Homework/Tester.cs
@@ -19,6 +19,52 @@ namespace TesterProgram
 
             Console.WriteLine("Mr. {0} {1}, is a middeling student who's gpa sits at {2} ID for the year is {3}", s1.FirstName, s1.LastName, s1.Gpa, s1.Id);
 
+            Console.WriteLine("\n\n");
+
+            Vehicle v1 = new Vehicle();
+            v1.Make = "Nissan";
+            v1.Model = "Quest";
+            v1.Year = Convert.ToInt16("2009");
+            v1.Weight = Convert.ToSingle("8000");
+
+            Console.WriteLine("The brand new {0} {1} {2}, coming in at only {3} pounds!!!", v1.Year, v1.Make, v1.Model, v1.Weight);
+
+            Console.WriteLine("\n\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
